71a4661b6a60ef649ae3fc87a4464f07cea5fb77
harshavardhan2712 <57551185+harshavardhan2712@users.noreply.github.com>
2022-01-30 19:14:19 +0530

    Changes for Sprint 1

--- src/main/java/com/optum/controller/AppController.java
+++ src/main/java/com/optum/controller/AppController.java
@@ -4,10 +4,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,51 +17,53 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.optum.model.Movie;
+
 import com.optum.model.FavouriteModel;
 import com.optum.model.User;
 import com.optum.service.FavouriteMovieService;
-import com.optum.service.LoginService;
+//import com.optum.service.LoginService;
 
 @RestController
 public class AppController {
-	
 
 	@Autowired
 	private FavouriteMovieService favouriteMovieService;
 
-	@Autowired
-	LoginService logInService;
-	
-	@PostMapping("/login")
-	public String logIn(User user) {
-		try {
-			boolean isAdded = logInService.addUser(user);
-			if (isAdded) {
-				return "User is Added..!!";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "User Not Added..!!";
-	}
-
-	@PostMapping("/signup")
-	public String SignUp(User user) {
-
-		try {
-			if (logInService.signUpUser(user)) {
-				return "SignUp Successfull";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "SignUp Failed might be username or password is incorrect";
-	}
+//	@Autowired
+//	LoginService logInService;
+//	
+//	@PostMapping("/login")
+//	public String logIn(User user) {
+//		try {
+//			boolean isAdded = logInService.addUser(user);
+//			if (isAdded) {
+//				return "User is Added..!!";
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return "User Not Added..!!";
+//	}
+//
+//	@PostMapping("/signup")
+//	public String SignUp(User user) {
+//
+//		try {
+//			if (logInService.signUpUser(user)) {
+//				return "SignUp Successfull";
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return "SignUp Failed might be username or password is incorrect";
+//	}
 
 	// http://localhost:9091/movieapp/deletefav/moviename
 	@DeleteMapping("deletefav/{title}")
@@ -75,28 +79,26 @@ public class AppController {
 	}
 
 	// http://localhost:9091/movieapp/addfav
-	@PostMapping("/addToFavourites")
-	public String addFavourite(@RequestBody FavouriteModel favouriteModel) {
-
+	@RequestMapping(value="/addToFavourites")
+	public void addFavourite(@RequestBody FavouriteModel favouriteModel) {
+		System.out.println("**************** fav model Title :" + favouriteModel.getTitle());
 		try {
 			favouriteMovieService.addFavCollection(favouriteModel);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "Movie has been Added into Favourite Collection";
+		//return "Movie has been Added into Favourite Collection";
 	}
 
-	
 	@GetMapping("/test")
 	public String test() {
-		
+
 		return "Hello";
 	}
-	
-	
+
 	@GetMapping("/results")
 	@ResponseBody
-	public ModelAndView searchMovies(@RequestParam("movie")String movieName) {
+	public ModelAndView searchMovies(@RequestParam("movie") String movieName) {
 		String url = "http://www.omdbapi.com/";
 		String charset = "UTF-8";
 		String query;
@@ -105,55 +107,65 @@ public class AppController {
 		modelAndview.setViewName("resultsPage.html");
 
 		try {
-			query = String.format("t=%s&apikey=e742800f", URLEncoder.encode(movieName, charset));
-			
-			URL url1= new URL(url + "?" + query);
-			HttpURLConnection conn = (HttpURLConnection)url1.openConnection(); 
+			query = String.format("s=%s&apikey=e742800f", URLEncoder.encode(movieName, charset));
+
+			URL url1 = new URL(url + "?" + query);
+			HttpURLConnection conn = (HttpURLConnection) url1.openConnection();
 			conn.setRequestMethod("GET");
 			conn.connect();
-			String msg = StreamUtils.copyToString( url1.openStream(), Charset.defaultCharset());
-			String[] data = msg.split(",");
-			
-			for(String d : data) {
-				if(d.contains("Title")) {
-					modelAndview.addObject("Title", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Year")) {
-					modelAndview.addObject("Year", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Rated")) {
-					modelAndview.addObject("Rated", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Released")) {
-					modelAndview.addObject("Released", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Actors")) {
-					modelAndview.addObject("Actors", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Director")) {
-					modelAndview.addObject("Director", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Writer")) {
-					modelAndview.addObject("Writer", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Language")) {
-					modelAndview.addObject("Language", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Country")) {
-					modelAndview.addObject("Country", d.split(":")[1].replace("\"", ""));
-				}
-				if(d.contains("Poster")) {
-					String[] poster = d.split(":");
-					if(poster.length >2) {
-					String posterImg = poster[1]+":"+poster[2];
-					modelAndview.addObject("Poster", posterImg.substring(1 , posterImg.length()-1));
-					}else {
-						modelAndview.addObject("Poster", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkmpiE3saxLv17jlQVpffuUAAtU95HJoaPRw&amp;usqp=CAU");
+			String msg = StreamUtils.copyToString(url1.openStream(), Charset.defaultCharset());
+			System.out.println("****************************" + msg);
+			String[] movieArray = msg.split("\\[");
+			String[] movieSummary = movieArray[1].split("\\}");
+			ArrayList<Movie> movie = new ArrayList<>();
+
+			for (String summary : movieSummary) {
+				String[] data = summary.split(",");
+				Movie movies = new Movie();
+				for (String d : data) {
+
+					if (d.contains("Title")) {
+						movies.setTitle(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Year")) {
+						movies.setYear(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Type")) {
+						movies.setRated(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Released")) {
+						movies.setReleased(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Actors")) {
+						movies.setActors(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Director")) {
+						movies.setDirector(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Writer")) {
+						movies.setWriter(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Language")) {
+						movies.setLanguage(d.split(":")[1].replace("\"", ""));
+					}
+					if (d.contains("Poster")) {
+						String[] poster = d.split(":");
+						if (poster.length > 2) {
+							String posterImg = poster[1] + ":" + poster[2];
+							movies.setPoster(posterImg.substring(1, posterImg.length() - 1));
+						} else {
+							movies.setPoster(
+									"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkmpiE3saxLv17jlQVpffuUAAtU95HJoaPRw&amp;usqp=CAU");
+						}
 					}
 				}
+				if (movies.getTitle() != null) {
+					movie.add(movies);
+				}
 			}
-			modelAndview.addObject("response", msg);
-			
+			modelAndview.addObject("favouriteModel", new FavouriteModel());
+			modelAndview.addObject("movielist", movie);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ src/main/java/com/optum/model/Movie.java
@@ -0,0 +1,99 @@
+package com.optum.model;
+
+public class Movie {
+	public Movie(String title, String year, String rated, String released, String director, String writer,
+			String actors, String language, String country, String poster) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.rated = rated;
+		this.released = released;
+		this.director = director;
+		this.writer = writer;
+		this.actors = actors;
+		this.language = language;
+		this.country = country;
+		this.poster = poster;
+	}
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+	private String title;
+	private String year;
+	private String rated;
+	private String released;
+	private String director;
+	private String writer;
+	private String actors;
+	private String language;
+	private String country;
+	private String poster;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getRated() {
+		return rated;
+	}
+	public void setRated(String rated) {
+		this.rated = rated;
+	}
+	public String getReleased() {
+		return released;
+	}
+	public void setReleased(String released) {
+		this.released = released;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getActors() {
+		return actors;
+	}
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	@Override
+	public String toString() {
+		return "Movie [title=" + title + ", year=" + year + ", rated=" + rated + ", released=" + released
+				+ ", director=" + director + ", writer=" + writer + ", actors=" + actors + ", language=" + language
+				+ ", country=" + country + ", poster=" + poster + "]";
+	}
+
+
+}
